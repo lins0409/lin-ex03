622d0793607842b873843c249de1dd9a9a7cb531
Sue Lin <r.lin13@knights.ucf.edu>
2021-09-04 14:20:22 -0400

    Initial commit

---
+++ src/main/java/exercise03/Solution03.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Sue Lin
+ */
+
+package exercise03;
+
+import java.util.Scanner;
+
+/*
+* this is the pseudocode
+* Create a program that prompts for a quote and an author. Display the quotation and author as shown in the example output.
+*   -print out prompt asking for string
+*   -create variable to store string
+*   -print out prompt asking for the author
+*   -create variable to store author name
+*   -print out the author and the quote
+ */
+
+public class Solution03 {
+    public static void main (String[] args)
+    {
+        System.out.print("What is the quote? ");
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        System.out.print("Who said it? ");
+        String author = scanner.nextLine();
+        char quote = '"';
+        System.out.print(author + " says, " + quote + string + quote);
+    }
+}
